d40269cbc857942ec8e3d58dfce130b70614a315
James Martelletti <james@martelletti.com.au>
2021-10-15 15:04:13 +1100

    Cleanup startup output

--- cmd/main.go
+++ cmd/main.go
@@ -31,22 +31,5 @@ func main() {
 
 // Print a beautiful banner
 func printBanner() {
-	fmt.Print("\033[31m")
-	fmt.Print(`  
-d8b                   
-Y8P                   
-                      
-888 88888b.   .d88b.  
-888 888 "88b d8P  Y8b 
-888 888  888 88888888 
-888 888 d88P Y8b.     
-888 88888P"   "Y8888  
-    888               
-    888               
-    888               
-`)
-	fmt.Println("\033[0m")
-	fmt.Println("\033[32mWelcome to Ipê - Yet another Pusher server clone (https://github.com/dimiro1/ipe)\033[0m")
-	fmt.Printf("\033[32mVersion %s+%s.git.%s\033[0m\n", version, buildstamp, githash)
-	fmt.Println("\033[33mBy: Claudemiro Alves Feitosa Neto <dimiro1@gmail.com>\033[0m")
+	fmt.Println("Websocket started")
 }
